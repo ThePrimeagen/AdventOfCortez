043117bd2c39adf054611b7cf32ce6af24f7b6d4
Michael Paulson <michael.b.paulson@gmail.com>
2019-12-05 23:49:50 -0800

    opti is still the worst mod. #facts

---
+++ d4q1.js
@@ -0,0 +1,26 @@
+/*
+ * It is a six-digit number.
+The value is within the range given in your puzzle input.
+Two adjacent digits are the same (like 22 in 122345).
+Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+*/
+function isCorrect(num) {
+    const nums = ("" + num).split('');
+
+    let rep = false;
+    let prev = nums[0];
+    let ordered = true;
+
+    for (let i = 1; i < nums.length && ordered; prev = nums[i++]) {
+        ordered = nums[i] >= prev;
+        rep = rep || prev === nums[i];
+    }
+    return rep && ordered;
+}
+
+let count = 0;
+for (let i = 248345; i < 746315; ++i) {
+    isCorrect(i) && count++;
+}
+
+console.log(count);
